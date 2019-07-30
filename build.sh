ca22ff77bf884ba09b0ac6d5d8aa55b7f369256a
Ubuntu <ubuntu@ip-10-2-0-33.ec2.internal>
2019-07-30 13:21:45 +0000

    implemented local building

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -f Dockerfile -t boodskapiot/platform:3.0.0-alpha1 -t boodskapiot/platform:latest .
+docker build -f Dockerfile.local -t boodskapiot/platform:3.0.0-alpha1 -t boodskapiot/platform:latest .
