287c82323994c3a3024ac65c2e40f749b0c2d943
Ubuntu <ubuntu@ip-10-2-0-33.ec2.internal>
2019-07-31 04:02:45 +0000

    synced api files before each build

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 cd distribution
 ./build.sh 
 cd ..
+cp ../boodskap-iot-platform3/iot-platform/iot-platform-apigen/src/main/resources/individual/* admin-console/yaml/
 docker build -f Dockerfile.local -t boodskapiot/platform:3.0.0-alpha1 -t boodskapiot/platform:latest .
 docker push boodskapiot/platform:latest
