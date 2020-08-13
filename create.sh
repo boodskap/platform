869c41cd92198c6d04fc46bc1692237119777163
Jegan Vincent <jvincent@boodskap.io>
2020-08-13 11:12:07 +0000

    Expossed mqtt port

--- create.sh
+++ create.sh
@@ -30,6 +30,6 @@ docker pull boodskapiot/gateway:${UI_VERSION}
 docker network create --subnet=10.1.1.0/24 platformnet
 docker container create --env-file cass-env.txt --privileged --net platformnet -p 9042:9042 --ip 10.1.1.3 --hostname cassandra --name cassandra -v $VOL_CASSANDRA:/root/data boodskapiot/cassandra:${CASSANDRA_VERSION}
 docker container create --env-file es-env.txt --privileged --net platformnet -p 9200:9200 --ip 10.1.1.4 --hostname elastic --name elastic -v $VOL_ELASTIC:/home/elastic/data boodskapiot/elastic:${ELASTIC_VERSION}
-docker container create --env-file bskp-env.txt --net platformnet -p 5555:5555/udp -p 18080:18080 -p 19090:19090 -p 2021:2021 --ip 10.1.1.2 --hostname boodskap --name boodskap -v $VOL_BOODSKAP/data:/root/data -v $VOL_BOODSKAP/work:/root/work boodskapiot/platform:${PLATFORM_VERSION}
+docker container create --env-file bskp-env.txt --net platformnet -p 1883:1883 -p 5555:5555/udp -p 18080:18080 -p 19090:19090 -p 2021:2021 --ip 10.1.1.2 --hostname boodskap --name boodskap -v $VOL_BOODSKAP/data:/root/data -v $VOL_BOODSKAP/work:/root/work boodskapiot/platform:${PLATFORM_VERSION}
 docker container create --net platformnet -p 80:80 --ip 10.1.1.254 --hostname gateway --name gateway boodskapiot/gateway:${UI_VERSION}
 #docker container create --net platformnet -p 5601:5601 --ip 10.1.1.6 --hostname kibana --name kibana -v $VOL_KIBANA:/home/kibana/data boodskapiot/kibana:${KIBANA_VERSION}
